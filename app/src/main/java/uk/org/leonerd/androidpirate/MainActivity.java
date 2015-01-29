e7eb60e160bee9c48c711b39417afc749bca517d
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-01-29 16:24:44 +0000

    Initial hack at sending messages in to Matrix

--- app/src/main/java/uk/org/leonerd/androidpirate/MainActivity.java
+++ app/src/main/java/uk/org/leonerd/androidpirate/MainActivity.java
@@ -3,6 +3,7 @@ package uk.org.leonerd.androidpirate;
 import android.content.Context;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +16,17 @@ import com.hoho.android.usbserial.driver.UsbSerialDriver;
 import com.hoho.android.usbserial.driver.UsbSerialPort;
 import com.hoho.android.usbserial.driver.UsbSerialProber;
 
+import org.matrix.androidsdk.MXDataHandler;
+import org.matrix.androidsdk.MXSession;
+import org.matrix.androidsdk.data.MXMemoryStore;
+import org.matrix.androidsdk.data.Room;
+import org.matrix.androidsdk.rest.callback.ApiCallback;
+import org.matrix.androidsdk.rest.client.LoginRestClient;
+import org.matrix.androidsdk.rest.model.Event;
+import org.matrix.androidsdk.rest.model.MatrixError;
+import org.matrix.androidsdk.rest.model.TextMessage;
+import org.matrix.androidsdk.rest.model.login.Credentials;
+
 import java.util.List;
 
 
@@ -22,8 +34,14 @@ public class MainActivity extends ActionBarActivity {
 
     private static final int MPL311A5_ADDR = 0x60;
 
+    private static final Uri HS_URI = Uri.parse("https://matrix.org");
+    // #test-bot:matrix.org
+    private static final String ROOM_ID = "!ewOgZEUrOZAAaQJNBv:matrix.org";
+
     TextView txtStatus;
     BusPirate mPirate;
+
+    MXSession mMatrixSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,16 +68,14 @@ public class MainActivity extends ActionBarActivity {
         try {
             mPirate.enterI2CMode();
             txtStatus.setText("Pirate I2C ready");
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             txtStatus.setText("Pirate failed: " + e);
             return;
         }
 
         try {
             mPirate.setPower(true);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             txtStatus.setText("Power failed: " + e);
             return;
         }
@@ -68,11 +84,46 @@ public class MainActivity extends ActionBarActivity {
             // CTRL_REG1 = ACTIVE
             mPirate.i2cSend(MPL311A5_ADDR, new byte[]{0x26, 0x01});
             txtStatus.setText("MPL311A5 ready");
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             txtStatus.setText("MPL311A5 failed: " + e);
             return;
         }
+
+    }
+
+    public void matrixLogin(View view) {
+        String user = ((TextView) findViewById(R.id.editTextLogin)).getText().toString();
+        String password = ((TextView) findViewById(R.id.editTextPassword)).getText().toString();
+
+        mMatrixSession = null;
+
+        new LoginRestClient(HS_URI).loginWithPassword(user, password,
+            new ApiCallback<Credentials>() {
+                @Override
+                public void onSuccess(Credentials credentials) {
+                    mMatrixSession = new MXSession(
+                            new MXDataHandler(new MXMemoryStore(), credentials),
+                            credentials
+                    );
+                    txtStatus.setText("Matrix logged in");
+                }
+
+                @Override
+                public void onNetworkError(Exception e) {
+                    txtStatus.setText("Matrix login failed: " + e);
+                }
+
+                @Override
+                public void onMatrixError(MatrixError e) {
+                    txtStatus.setText("Matrix login failed: " + e);
+                }
+
+                @Override
+                public void onUnexpectedError(Exception e) {
+                    txtStatus.setText("Matrix login failed: " + e);
+                }
+            }
+        );
     }
 
     @Override
@@ -112,6 +163,27 @@ public class MainActivity extends ActionBarActivity {
 
         ((TextView)findViewById(R.id.txtPressure)).setText(String.format("%.2f Pa", pressure));
         ((TextView)findViewById(R.id.txtTemperature)).setText(String.format("%.3f C", temperature));
+
+        if (mMatrixSession != null) {
+            Room room = mMatrixSession.getDataHandler().getRoom(ROOM_ID);
+            TextMessage message = new TextMessage();
+            message.body = String.format("Sensor reading: pressure=%.2f Pa, temperature=%.3f C",
+                    pressure, temperature);
+
+            room.sendMessage(message, new ApiCallback<Event>() {
+                @Override
+                public void onSuccess(Event info) { }
+
+                @Override
+                public void onNetworkError(Exception e) { }
+
+                @Override
+                public void onMatrixError(MatrixError e) { }
+
+                @Override
+                public void onUnexpectedError(Exception e) { }
+            });
+        }
     }
 
     private BusPirate findBusPirate() {
